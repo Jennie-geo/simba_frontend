1b7874f7b589ca40465afbbd48157c5f94e6c9d3
Jennie-geo <isintumejenny@gmail.com>
2022-04-16 00:22:57 +0100

    added a modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import useAxios from 'axios-hooks';
+import { contextObj } from './Context';
 import '../index.css';
 import './Modal.css';
 
 const Modal = ({ closeModal }) => {
+    const contextData = useContext(contextObj);
+    const [axiosUserResponse] = useAxios({
+        url: `${process.env.REACT_APP_BACKEND_URL}/user/getUser`,
+        headers: { 'Authorization': `bearer ${contextData.data.token}` }
+    })
+
+    let userData = []
+    if (!axiosUserResponse.error) {
+        userData = axiosUserResponse.data.user;
+
+    }
+
     return <div className='modalBackground' >
         <div className='modalContainer' style={{ backgroundColor: "white", color: "black" }}>
             <div className='titleCloseBtn'>
                 <button onClick={() => closeModal(false)}> X </button>
             </div>
             <div className='title'><h2>Select an account name below</h2></div>
-            <div className="body">
-                <div className='firstName'>Jennifer</div>
-                <div className='lastName'>Isintume</div>
-            </div>
+            {userData.map(user => (
+                <div className="body" >
+                    <div className='firstName' style={{ marginRight: "12px" }}>{user.firstName}</div>
+                    <div className='lastName'>{user.lastName}</div>
+                </div>
+            ))}
             <div className='footer'>
                 <button onClick={() => closeModal(false)} id="cancelBtn">Cancel</button>
                 <button>Continue</button>
